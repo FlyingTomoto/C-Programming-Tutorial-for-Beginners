c71cd1aa34eeaa0427ac8b120d5b454be7a0c66c
FlyingTomoto <67462776+FlyingTomoto@users.noreply.github.com>
2021-09-29 16:26:19 +0200

    Update and rename main.c to hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,11 @@
+/* file is named hello-world.c in VS Code - to run, go to terminal, type "make hello-world", once complied, type "./hello-world" to get the output. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("hello world!\n");
+    
+    return 0;
+}
